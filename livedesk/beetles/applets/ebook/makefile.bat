9eec1538c9805d4f85a07715cfdeaf218d609ae8
Johnny Guo <shuntianxia@qq.com>
2018-10-13 16:00:52 +0800

    删除dv ebook fm movie record

--- livedesk/beetles/applets/ebook/makefile.bat
+++
@@ -1,7 +0,0 @@
-call pk mbook
-call pk mdata
-call cc app_ebook
-call cc page_com
-call cc ebook_uipara
-call cc ebook_mset
-call cc ebook_sset
--- livedesk/beetles/applets/ebook/mbook/makefile.bat
+++
@@ -1,6 +0,0 @@
-call cc mbook_decode
-call cc mbook_show
-call cc mbook_analysis
-call cc mbook_layer
-call cc mbook_font
-call cc mbook_npl
